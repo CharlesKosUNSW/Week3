e503d8262d9b14cb1cac2148376180f4ac3c2efe
CharlesKosUNSW <z5160675@ad.unsw.edu.au>
2022-06-15 18:08:16 +1000

    Class, click here

--- Week3/t1.c
+++ Week3/t1.c
@@ -1,8 +1,35 @@
+// Charles Kos
+// 15.06.2022
+// z5160675@unsw.edu.au
+// Copied from tutorial questions for COMP1911 W2
+
+// Question: what will the output be? Will the if statement do anything different to the while loop?
+
 #include <stdio.h>
 
-int main(void) {
+void t1(void) {
 
-	printf("Pls be working\n");
+	//Define variables
+	int inputNum, i;
+
+	//Get user input
+	printf("Enter a number: ");
+	scanf("%d", &inputNum);
+
+	//Store input in 
+	i = inputNum;
+
+	//If statement
+	//if (i <= 5) {
+	//	printf("%d\n", i * i);
+	//	i++;
+	//}
+
+	// While loop
+	while (i <= 5) {
+		printf("%d\n", i * i);
+		i++;
+	}
 
 	return 0;
 }
---
+++ Week3/w3main.c
@@ -0,0 +1,18 @@
+// Charles Kos
+// 15.06.2022
+// z5160675@unsw.edu.au
+// Main function for running the tutorial questions
+
+// STUDENTS IGNORE THIS (it lets Visual Studio run scanf() without an error)
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#include <stdio.h>
+
+int main(void) {
+
+	t1();
+
+	return 0;
+}
